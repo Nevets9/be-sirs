642bd31856a8401fa827f64497a3231409935636
Steven <stevenbae9@gmail.com>
2025-05-04 21:54:49 +0700

    test; testing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 // 3. ROUTE
 app.use('/api/v1/users', userRouter);
-app.use('/', console.log("hai"));
+app.get('/', (req, res) => res.send('OK'));
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
